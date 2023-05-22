354c115000f46854dfef0a62d1635b902d286a28
MamaKbubu <maboelatk@gmail.com>
2023-05-22 16:22:15 +0200

    try 3 task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,28 +4,40 @@
 import requests
 import sys
 
+def get_employee_todo_progress(employee_id):
+    base_url = 'https://jsonplaceholder.typicode.com'
+    employee_url = f'{base_url}/users/{employee_id}'
+    todos_url = f'{base_url}/todos?userId={employee_id}'
 
-if __name__ == '__main__':
-    employeeId = sys.argv[1]
-    baseUrl = "https://jsonplaceholder.typicode.com/users"
-    url = baseUrl + "/" + employeeId
+    # Fetch employee data
+    response = requests.get(employee_url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve employee data. Error: {response.status_code}")
+        return
 
-    response = requests.get(url)
-    employeeName = response.json().get('name')
+    employee_data = response.json()
+    employee_name = employee_data['name']
 
-    todoUrl = url + "/todos"
-    response = requests.get(todoUrl)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
+    # Fetch TODO list for the employee
+    response = requests.get(todos_url)
+    if response.status_code != 200:
+        print(f"Failed to retrieve TODO list data. Error: {response.status_code}")
+        return
 
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+    todos_data = response.json()
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(employeeName, done, len(tasks)))
+    # Calculate progress
+    total_tasks = len(todos_data)
+    completed_tasks = [task for task in todos_data if task['completed']]
+    num_completed_tasks = len(completed_tasks)
 
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+    # Display progress
+    print(f"Employee {employee_name} is done with tasks ({num_completed_tasks}/{total_tasks}):")
+
+    for task in completed_tasks:
+        print(f"\t{task['title']}")
+
+# Usage example
+employee_id = 1  # Replace with the desired employee ID
+get_employee_todo_progress(employee_id)
+
